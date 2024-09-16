755e1393097a78cfb0afd63bc73acc73a1a715a4
abdullahajhamna <168464879+abdullahajhamna@users.noreply.github.com>
2024-09-16 10:34:32 +0300

    Quiz App

---
+++ quiz_data.py
@@ -0,0 +1,52 @@
+quiz_data=[
+    {
+        "question": "What is the capital of Jordan?",
+        "choices": ["Amman", "Irbid", "Zarqa", "Aqaba"],
+        "answer": "Amman"
+    },
+    {
+        "question": "Which significant archaeological site, one of the New Seven Wonders of the World, is located in Jordan?",
+        "choices": ["Petra", "Jerash", "Wadi Rum", "Al-Maghtas"],
+        "answer": "Petra"
+    },
+    {
+        "question": "What river forms part of Jordan's western border?",
+        "choices": ["Euphrates", "Nile", "Jordan", "Tigris"],
+        "answer": "Jordan"
+    },
+    {
+        "question": "Which historical fortress in Jordan is known for its Crusader castle?",
+        "choices": ["Ajloun Castle", "Kerak Castle", "Shobak Castle", "All of the above"],
+        "answer": "All of the above"
+    },
+    {
+        "question": "What is the official language of Jordan?",
+        "choices": ["Arabic", "English", "French", "Turkish"],
+        "answer": "Arabic"
+    },
+    {
+        "question": "Which city is known as the cultural capital of Jordan?",
+        "choices": ["Amman", "Irbid", "Ma'an", "Aqaba"],
+        "answer": "Irbid"
+    },
+    {
+        "question": "What is the currency of Jordan?",
+        "choices": ["Dinar", "Pound", "Dollar", "Euro"],
+        "answer": "Dinar"
+    },
+    {
+        "question": "Which desert in Jordan is famous for its unique rock formations and landscapes?",
+        "choices": ["Rub' al Khali", "Wadi Rum", "Great Sandy Desert", "Dasht-e Kavir"],
+        "answer": "Wadi Rum"
+    },
+    {
+        "question": "What is the name of the famous archaeological city that is a UNESCO World Heritage Site located in Jordan?",
+        "choices": ["Jerash", "Petra", "Um Qais", "Pella"],
+        "answer": "Petra"
+    },
+    {
+        "question": "Jordan shares borders with which three countries?",
+        "choices": ["Saudi Arabia, Iraq, Israel", "Syria, Iraq, Palestine", "Egypt, Saudi Arabia, Syria", "Israel, Syria, Lebanon"],
+        "answer": "Syria, Iraq, Palestine"
+    }
+]
